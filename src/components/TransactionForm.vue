faa7847d7939fc160636e0d3682f058eb024d761
Rousbel <rousbel_01@hotmail.com>
2025-05-18 13:40:36 -0700

    TransactionForm.vue converting it to typescript

--- src/components/TransactionForm.vue
+++ src/components/TransactionForm.vue
@@ -5,7 +5,7 @@
   >
     <div>
       <label class="block">Type</label>
-      <select v-model="type" class="input" required>
+      <select v-model="form.type" class="input" required>
         <option value="income">Income</option>
         <option value="expense">Expense</option>
       </select>
@@ -13,30 +13,38 @@
     
     <div>
       <label class="block">Amount</label>
-      <input v-model.number="amount" type="number" class="input" required />
+      <input v-model.number="form.amount" type="number" class="input" required />
     </div>
 
 
     <div>
       <label>Category:</label>
-      <select v-model="selectedDefaultCategory" class="input" placeholder="Default">
+      <select v-model="form.category" class="input" placeholder="Default">
         <option disabled value="">Select a category</option>
         <option
-          v-for="cat in defaultCategories"
+          v-for="cat in categories"
           :key="cat.name"
-          :value="cat"
+          :value="cat.name"
         >
           {{ cat.icon }} {{ cat.name }}
         </option>
       </select>
-      <div class="pt-3">
-        <input v-model="selectedCustomCategory" type="text" class="input" placeholder="Custom"/>
+
+      <div>
+        <button type="button" class="text-blue-600 underline" @click="show_add_category =!show_add_category">
+          + Add New Category
+        </button>
       </div>
-    </div>
+
+      <div v-if="show_add_category" class="flex gap-2 items-center">
+        <input v-model="new_category.name" placeholder="Category name" class="input" />
+        <Button label="Add"  @click="addCategory"severity="success" :disabled="!new_category.name"/>
+      </div>
+      </div>
 
     <div>
       <label class="block">Description</label>
-      <input v-model="description" type="text" class="input" />
+      <input v-model="form.description" type="text" class="input" />
     </div>
 
     <button
@@ -48,81 +56,79 @@
   </form>
 </template>
 
-<script>
+<script lang="ts" setup>
+
+import Button from 'primevue/button';
+import { reactive, ref, onMounted } from 'vue';
 import API from "../api";
 
-export default {
-  data() {
-    return {
-      amount: "",
-      type: "expense",
-      category: "",
-      description: "",
-      categoryInput: "",
-      selectedDefaultCategory: "",
-      selectedCustomCategory:"",
-      defaultCategories: [],
-      customCategories: []
-    };
-  },
-  computed: {
-    computedCategory() {
-      return this.categoryMode === 'custom'
-        ? { name: this.categoryInput.trim(), icon: '📝' }
-        : this.selectedDefault;
-    }
-  },
-  methods: {
-    async addCustomCategory(category){
-      try {
-        await API.post('/categories',{
-          id: category.id,
-          name: category.name,
-          icon: category.icon,
-          is_default: category.is_default
-        })
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async submitTransaction() {
-      if (!this.computedCategory?.name) {
-        alert('Please select or enter a category.');
-        return;
-      }
-
-      if(!this.computedCategory?.is_default){
-        this.addCustomCategory(this.computedCategory)
-      }
-
-      try {
-        await API.post("/transactions", {
-          amount: this.amount,
-          type: this.type,
-          category: this.computedCategory.name,
-          description: this.description,
-        });
-
-        this.$emit("submitted");
-        this.amount = "";
-        this.categoryInput = "";
-        this.selectedDefault = "";
-        this.description = "";
-      } catch (err) {
-        console.error(err);
-        alert("Error adding transaction.");
-      }
-    },
-    async fetchCategories(){
-      const res = await API.get('/categories')
-      this.defaultCategories = res.data.filter(cat=> cat.is_default);
-      this.customCategories = res.data.filter(cat=> !cat.is_default);
-    }
-  },
-  mounted(){
-    this.fetchCategories();
-  }
+interface TransactionFormData{
+  type: "income" | "expense";
+  category:string;
+  description: string;
+  amount:number;
 };
+
+interface Category{
+  name: string;
+  icon: string;
+  is_default: boolean;
+};
+
+const categories = ref<Category[]>([]);
+const show_add_category = ref(false);
+const new_category = reactive<Category>({
+    name: "",
+    icon: "📝",
+    is_default: false
+});
+
+const form = reactive<TransactionFormData>({
+  type: "expense",
+  amount:0,
+  description:'',
+  category: ""
+});
+
+  const  addCategory =  async () => {
+        try {
+        const res = await API.post('/categories/add', new_category)
+          show_add_category= false;
+          categories.push(res.data);
+          new_category = "";
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+  const submitTransaction = async () => {
+        if (!form.category) {
+          console.log("selected category",form.category)
+          alert('Please select or enter a category.');
+          return;
+        }
+
+        try {
+          await API.post("/transactions/add", form);
+          form.amount = "";
+          form.category = "";
+          form.description = "";
+          form.category = "";
+
+        } catch (err) {
+          console.error(err);
+          alert("Error adding transaction.");
+        }
+    }
+
+  const fetchCategories = async() => {
+        const res = await API.get('/categories')
+        categories.value = res.data;
+  }
+
+  onMounted(() => {
+    fetchCategories()
+  })
 </script>
 
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './assets/index.css';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
+const app = createApp(App);
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: false,
+            cssLayer: false
+        }
+    }
+}).mount('#app');
 
-createApp(App).mount('#app')
